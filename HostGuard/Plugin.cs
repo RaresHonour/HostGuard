d5109ee5533b443242985096f4ad532b18ba6fbe
RaresHonour <rares.herinean@gmail.com>
2026-04-09 16:52:18 +0300

    Restructure project into HostGuard subfolder and bump version to 1.2.1
    Add solution file and NuGet config for multi-project layout.

--- HostGuard/Plugin.cs
+++ HostGuard/Plugin.cs
@@ -4,7 +4,7 @@ using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Reactor;
 
-[BepInPlugin("com.rareshonour.hostguard", "HostGuard", "1.2.0")]
+[BepInPlugin("com.rareshonour.hostguard", "HostGuard", "1.2.1")]
 [BepInDependency(ReactorPlugin.Id)]
 public class HostGuardPlugin : BasePlugin
 {
@@ -17,7 +17,7 @@ public class HostGuardPlugin : BasePlugin
         HostGuardConfig.Initialize(Config);
         _harmony = new Harmony("com.rareshonour.hostguard");
         _harmony.PatchAll();
-        Logger.LogInfo("HostGuard 1.2.0 loaded.");
+        Logger.LogInfo("HostGuard 1.2.1 loaded.");
         Logger.LogInfo($"[HostGuard] Config: BanInsteadOfKick={HostGuardConfig.BanInsteadOfKick.Value}, ContainsMode={HostGuardConfig.ContainsMode.Value}, AnnounceKick={HostGuardConfig.AnnounceKick.Value}");
         Logger.LogInfo($"[HostGuard] Config: BannedWords=[{HostGuardConfig.BannedWords.Value}], BadNameWords=[{HostGuardConfig.BadNameWords.Value}]");
         Logger.LogInfo($"[HostGuard] Config: Whitelist=[{HostGuardConfig.WhitelistedCodes.Value}], BanListUrl={(!string.IsNullOrEmpty(HostGuardConfig.BanListUrl.Value) ? "set" : "not set")}");
